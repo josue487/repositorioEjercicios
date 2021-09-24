2ac81ba1a04e1a6c34ee953a5665e713346d6567
Franco Josue Matorras <josue487@gmail.com>
2021-09-24 12:24:59 -0300

    Leccion 1 Kotlin : Variables y Constantes

--- app/src/main/java/com/ice/repositorioejercicios/MainActivity.kt
+++ app/src/main/java/com/ice/repositorioejercicios/MainActivity.kt
@@ -7,5 +7,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        variablesYConstantes()
+    }
+
+    /*
+    Aqui vamos a hablar de variables y constantes
+     */
+    private fun variablesYConstantes(){
+        //Variables
+        var myFirstVariable = "Hola Mundo"
+        println(myFirstVariable)
+        myFirstVariable = "Bienvenido"
+        println(myFirstVariable)
+        //No podemos asignar un int a una variable string
+        //myFirstVariable = 1
+        var mySecondVariable = "Suscribete"
+        println(mySecondVariable)
+        mySecondVariable = myFirstVariable
+        myFirstVariable="ya te has suscrito?"
+        println(myFirstVariable)
+        println("no")
+
+        //Constantes
+
+        val myFirstConstant = "te ha gustado el tutorial?"
+        println(myFirstConstant)
+        //una constante no puede modificar su valor
+        //myFirstConstant = ":D"
+        val mySecondConstant = myFirstVariable
+        println(mySecondConstant)
     }
 }
